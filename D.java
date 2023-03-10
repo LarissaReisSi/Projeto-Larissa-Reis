e977c276beba75520bb352a43ba04903a3ea5a9e
LarissaReisSi <108767040+LarissaReisSi@users.noreply.github.com>
2023-03-10 11:28:09 -0300

    Criado o método MD4

---
+++ D.java
@@ -0,0 +1,20 @@
+public class D {
+    private int D1;
+    private int D2;
+
+    public void MD1(){
+        System.out.println("MD1");
+    }
+
+    public void MD2(){
+        System.out.println("MD2");
+    }
+    
+    public void MD3(){
+        System.out.println("MD3");
+    }
+
+    public void MD4(){
+        System.out.println("MD4");
+    }
+}
