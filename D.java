a0073ba825c6c2d2a33465d1cff62269183a5d01
LarissaReisSi <108767040+LarissaReisSi@users.noreply.github.com>
2023-03-10 11:44:54 -0300

    Merge solucionado

--- D.java
+++ D.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class D {
     private int D1;
     private int D2;
@@ -14,22 +13,9 @@ public class D {
     public void MD3(){
         System.out.println("MD3");
     }
-}
-=======
-public class D {
-    private int D1;
-    private int D2;
-
-    public void MD1(){
-        System.out.println("MD1");
-    }
-
-    public void MD2(){
-        System.out.println("MD2");
-    }
 
     public void MD4(){
         System.out.println("MD4");
     }
 }
->>>>>>> a1fcd659fdc9c8cd520f9633bd6fe8878997b6a6
+
